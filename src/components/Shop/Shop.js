c0b7588b6acecda80f18e8361cf1689ff0cc8334
Akib Rayhan <kaziakibrayhan2018@gmail.com>
2022-11-08 11:26:37 +0600

    display product img

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Product from "../Product/Product";
 import "./Shop.css";
 
 const Shop = () => {
@@ -11,7 +12,9 @@ const Shop = () => {
   return (
     <div className="shop-container">
       <div className="products-container">
-        <h2>Lorem ipsum dolor sit amet.{products.length}</h2>
+        {products.map((product) => (
+          <Product key={product.id} product={product}></Product>
+        ))}
       </div>
       <div className="cart-container">
         <h2>Lorem ipsum dolor sit amet.</h2>
