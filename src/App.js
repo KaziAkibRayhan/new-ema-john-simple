d32d5a092b0cd17b6a594ae294d09f2fefbed9eb
Akib Rayhan <kaziakibrayhan2018@gmail.com>
2022-11-16 23:08:41 +0600

    react-router-dom added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
-import Header from "./components/Header/Header";
-import Shop from "./components/Shop/Shop";
+import Main from "./layouts/Main";
 
 function App() {
+  const router = createBrowserRouter([{ path: "/", element: <Main></Main> }]);
   return (
     <div className="App">
-      <Header></Header>
-      <Shop />
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
