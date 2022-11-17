b25fc81e5ca1878bc1a4e33684e2c874de5b63c6
Akib Rayhan <kaziakibrayhan2018@gmail.com>
2022-11-17 11:03:01 +0600

    route by children and outlet added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./About/About";
 import "./App.css";
+import Inventory from "./components/Inventory/Inventory";
+import Orders from "./components/Orders/Orders";
+import Shop from "./components/Shop/Shop";
 import Main from "./layouts/Main";
 
 function App() {
-  const router = createBrowserRouter([{ path: "/", element: <Main></Main> }]);
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children: [
+        { path: "/", element: <Shop></Shop> },
+        {
+          path: "/orders",
+          loader: () => fetch("products.json"),
+          element: <Orders></Orders>,
+        },
+        { path: "/inventory", element: <Inventory></Inventory> },
+        { path: "/about", element: <About></About> },
+      ],
+    },
+  ]);
   return (
     <div className="App">
       <RouterProvider router={router}></RouterProvider>
