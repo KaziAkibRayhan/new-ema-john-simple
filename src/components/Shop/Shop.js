ea9cd65fda2788be49f63b111f7c213b7b089226
kazi Akib Rayhan <kaziakibrayhan2018@gmail.com>
2023-01-01 16:59:31 +0600

    Set Page and Size state and send search query to server

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -10,11 +10,23 @@ import Product from "../Product/Product";
 import "./Shop.css";
 
 const Shop = () => {
-  const { products, count } = useLoaderData();
+  // const { products, count } = useLoaderData();
+  const [products, setProducts] = useState([]);
+  const [count, setCount] = useState(0);
   const [cart, setCart] = useState([]);
   const [page, setPage] = useState(0);
   const [size, setSize] = useState(10);
   const pages = Math.ceil(count / size);
+
+  useEffect(() => {
+    const url = `http://localhost:5000/products?page=${page}&size=${size}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setCount(data.count);
+        setProducts(data.products);
+      });
+  }, [page, size]);
 
   const clearCart = () => {
     setCart([]);
@@ -71,12 +83,26 @@ const Shop = () => {
         </Cart>
       </div>
       <div className="pagination">
-        <p>Currently selected page: {page}</p>
+        <p>
+          Currently selected page: {page} and Size: {size}
+        </p>
         {[...Array(pages).keys()].map((number) => (
-          <button className={page === number && 'selected'} key={number} onClick={() => setPage(number)}>
+          <button
+            className={page === number && "selected"}
+            key={number}
+            onClick={() => setPage(number)}
+          >
             {number}
           </button>
         ))}
+        <select onChange={(event) => setSize(event.target.value)}>
+          <option value="5">5</option>
+          <option value="10" selected>
+            10
+          </option>
+          <option value="15">15</option>
+          <option value="20">20</option>
+        </select>
       </div>
     </div>
   );
